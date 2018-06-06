a19951fd12fa5994ce103047924afc37db49f164
Viljami Nurminen <axel.viljami.nurminen@hotmail.com>
2018-06-06 22:52:34 +0300

    MainActivity now gives a collection of restaurants for DataFetchTasks to fetch

--- app/src/main/java/com/example/axelv/lunchlist/MainActivity.java
+++ app/src/main/java/com/example/axelv/lunchlist/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.example.axelv.lunchlist.model.Menu;
 import com.example.axelv.lunchlist.model.Restaurant;
+import com.example.axelv.lunchlist.model.RestaurantType;
 import com.example.axelv.lunchlist.parsers.AmicaParser;
 import com.example.axelv.lunchlist.parsers.SodexoParser;
 import com.example.axelv.lunchlist.views.RestaurantView;
@@ -45,27 +46,31 @@ public class MainActivity extends AppCompatActivity implements ResultHandler{
             String[] amicaIDs = {"0190", "0199"}; // Alvari, TUAS
             String[] sodexoIDs = {"142", "26521","13918", "140"}; // T-talo, Kvarkki, Valimo, Konetekniikka
 
+            Tuple<URL, Integer>[] restaurants = new Tuple[amicaIDs.length + sodexoIDs.length];
+            for(int i = 0; i < amicaIDs.length; i++ ) {
+                String url = String.format("http://www.amica.fi/modules/json/json/Index?costNumber=%s&language=fi&firstDay=%s",amicaIDs[i], amicaDate);
+                restaurants[i] = new Tuple<>(new URL(url), RestaurantType.AMICA);
+            }
+            for(int j = 0; j < sodexoIDs.length; j++ ) {
+                String url = String.format("https://www.sodexo.fi/ruokalistat/output/weekly_json/%s/%s/fi",sodexoIDs[j], sodexoDate);
+                restaurants[amicaIDs.length + j] = new Tuple<>(new URL(url), RestaurantType.SODEXO);
+            }
+
+            for(Tuple<URL, Integer> tuple: restaurants){
+                Log.i("LunchList", tuple.first().toString() + " - " + tuple.second());
+            }
+
            // Log.i("LunchList", amicaFormat.format(calendar.getTime()));
             // Initialize and execute the AsyncTask fetching the data
             DataFetchTask dft = new DataFetchTask();
             dft.handler = this;
-            URL[] amicaURLs = new URL[amicaIDs.length];
-            URL[] sodexoURLs = new URL[sodexoIDs.length];
-
-            /*for(int i = 0; i < amicaUrls.length; i++){
-                amicaUrls[i] = new URL(
-                    String.format("http://www.amica.fi/modules/json/json/Index?costNumber=%s&language=fi&firstDay=%s", amicaIDs[i], amicaDate)
-                );
-            }*/
-            for(int j = 0; j < sodexoURLs.length; j++){
-                sodexoURLs[j] = new URL(
-                        String.format("https://www.sodexo.fi/ruokalistat/output/weekly_json/%s/%s/fi",sodexoIDs[j], sodexoDate)
-                );
-            }
 
 
-            //dft.execute(new URL("http://www.amica.fi/modules/json/json/Index?costNumber=0190&language=fi&firstDay=" + sdf.format(calendar.getTime())));
-            dft.execute(sodexoURLs);
+
+
+
+
+            dft.execute(restaurants);
             // Initialize Sodexo reguest
 
 
@@ -80,14 +85,24 @@ public class MainActivity extends AppCompatActivity implements ResultHandler{
 
 
     }
-    public void changeText(String s) {
+    public void changeText(Restaurant[] restaurants) {
         //TextView tw = findViewById(R.id.textview);
-        AmicaParser amicaParser = new AmicaParser();
+        /*AmicaParser amicaParser = new AmicaParser();
         SodexoParser sodexoParser = new SodexoParser();
-        Restaurant restaurant = sodexoParser.parse(s);
+        Restaurant restaurant = sodexoParser.parse(s);*/
 
+        // Hide the loading text
+       // TextView loadingText = findViewById(R.id.loadingText);
+       // loadingText.setText("");
 
+        //Find the linear layout in which to put the restaurants
         LinearLayout layout =  findViewById(R.id.linearLayout);
-        layout.addView(new RestaurantView(restaurant, getApplicationContext()));
+        for(Restaurant restaurant : restaurants)
+            layout.addView(new RestaurantView(restaurant, getApplicationContext()));
+    }
+
+    public void updateProgress(int i){
+        //TextView loadingText = findViewById(R.id.loadingText);
+        //loadingText.setText("Loading... " + i + "/6" );
     }
 }
